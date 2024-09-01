fe77eb5e9b6e9fb0810f733430159b88e7300461
Yevhenii Baranik <baranik18@gmail.com>
2024-09-01 16:22:55 +0100

    init

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -6,43 +6,50 @@ let formData = {
 };
 
 const form = document.querySelector('.feedback-form');
-const emailInput = form.elements.email;
-const messageInput = form.elements.message;
 
+// Перевіряємо, чи існує форма на сторінці
+if (form) {
+	const emailInput = form.elements.email;
+	const messageInput = form.elements.message;
 
-loadFormData();
+	// Завантажуємо дані з localStorage при завантаженні сторінки
+	loadFormData();
 
-form.addEventListener('input', onFormInput);
-form.addEventListener('submit', onFormSubmit);
+	form.addEventListener('input', onFormInput);
+	form.addEventListener('submit', onFormSubmit);
 
-function onFormInput(event) {
-	formData[event.target.name] = event.target.value.trim();
-	localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
-
-
-function onFormSubmit(event) {
-	event.preventDefault();
-
-	if (emailInput.value === '' || messageInput.value === '') {
-		alert('Fill please all fields');
-		return;
+	function onFormInput(event) {
+		formData[event.target.name] = event.target.value.trim();
+		localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
 	}
 
-	console.log(formData);
+	function onFormSubmit(event) {
+		event.preventDefault();
 
-	localStorage.removeItem(STORAGE_KEY);
-	formData = { email: '', message: '' };
-	form.reset();
-}
+		// Перевірка, чи всі поля заповнені
+		if (emailInput.value === '' || messageInput.value === '') {
+			alert('Fill please all fields');
+			return;
+		}
 
+		console.log(formData);
 
-function loadFormData() {
-	const savedData = localStorage.getItem(STORAGE_KEY);
-
-	if (savedData) {
-		formData = JSON.parse(savedData);
-		emailInput.value = formData.email;
-		messageInput.value = formData.message;
+		// Очищення localStorage та formData
+		localStorage.removeItem(STORAGE_KEY);
+		formData = { email: '', message: '' };
+		form.reset();
 	}
+
+	function loadFormData() {
+		const savedData = localStorage.getItem(STORAGE_KEY);
+
+		// Якщо є збережені дані, підставляємо їх у форму
+		if (savedData) {
+			formData = JSON.parse(savedData);
+			emailInput.value = formData.email;
+			messageInput.value = formData.message;
+		}
+	}
+} else {
+	console.error('Форма не знайдена на сторінці.');
 }
