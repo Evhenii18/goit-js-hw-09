524b65655ae8e132f84a565427b780c25233d38d
Yevhenii Baranik <baranik18@gmail.com>
2024-09-01 18:14:01 +0100

    init

--- src/js/1-gallery.js
+++ src/js/1-gallery.js
@@ -7,71 +7,76 @@ const images = [
 	},
 
 	{
-		preview: new URL('../img/1.jpg', import.meta.url).href,
-		original: new URL('../img/1.jpg', import.meta.url).href,
+		preview: new URL('../img/2.jpg', import.meta.url).href,
+		original: new URL('../img/2.jpg', import.meta.url).href,
 		description: 'Image 2 description',
 	},
 	{
-		preview: new URL('../img/1.jpg', import.meta.url).href,
-		original: new URL('../img/1.jpg', import.meta.url).href,
+		preview: new URL('../img/3.jpg', import.meta.url).href,
+		original: new URL('../img/3.jpg', import.meta.url).href,
 		description: 'Image 3 description',
 	},
 	{
-		preview: new URL('../img/1.jpg', import.meta.url).href,
-		original: new URL('../img/1.jpg', import.meta.url).href,
+		preview: new URL('../img/4.jpg', import.meta.url).href,
+		original: new URL('../img/4.jpg', import.meta.url).href,
 		description: 'Image 4 description',
 	},
 	{
-		preview: new URL('../img/1.jpg', import.meta.url).href,
-		original: new URL('../img/1.jpg', import.meta.url).href,
+		preview: new URL('../img/5.jpg', import.meta.url).href,
+		original: new URL('../img/5.jpg', import.meta.url).href,
 		description: 'Image 5 description',
 	},
 	{
-		preview: new URL('../img/1.jpg', import.meta.url).href,
-		original: new URL('../img/1.jpg', import.meta.url).href,
+		preview: new URL('../img/6.jpg', import.meta.url).href,
+		original: new URL('../img/6.jpg', import.meta.url).href,
 		description: 'Image 6 description',
 	},
 	{
-		preview: new URL('../img/1.jpg', import.meta.url).href,
-		original: new URL('../img/1.jpg', import.meta.url).href,
+		preview: new URL('../img/7.jpg', import.meta.url).href,
+		original: new URL('../img/7.jpg', import.meta.url).href,
 		description: 'Image 7 description',
 	},
-	
 	{
-		preview: new URL('../img/1.jpg', import.meta.url).href,
-		original: new URL('../img/1.jpg', import.meta.url).href,
+		preview: new URL('../img/8.jpg', import.meta.url).href,
+		original: new URL('../img/8.jpg', import.meta.url).href,
+		description: 'Image 8 description',
+	},
+	{
+		preview: new URL('../img/9.jpg', import.meta.url).href,
+		original: new URL('../img/9.jpg', import.meta.url).href,
 		description: 'Image 9 description',
 	},
 	
 ];
 
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const galleryContainer = document.querySelector('.gallery');
+
 const galleryMarkup = images.map(({ preview, original, description }) => {
 	return `
     <li class="gallery-item">
-      <a class="gallery-link" href="${original}">
-        <img
-          class="gallery-image"
-          src="${preview}"
-          data-source="${original}"
-          alt="${description}"
-        />
-      </a>
-    </li>`;
+        <a class="gallery-link" href="${original}">
+            <img 
+                class="gallery-image" 
+                src="${preview}" 
+                alt="${description}" 
+            />
+        </a>
+    </li>
+    `;
 }).join('');
 
 galleryContainer.innerHTML = galleryMarkup;
+
 
 const lightbox = new SimpleLightbox('.gallery a', {
 	captions: true,
 	captionsData: 'alt',
 	captionPosition: 'bottom',
 	captionDelay: 250,
-	overlay: true,
 });
+
 
 
